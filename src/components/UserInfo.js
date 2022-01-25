baea811c7c185acf1a57852d257470e54a20f011
Adrien GOGOIS <ad.gogois@gmail.com>
2022-01-25 15:58:02 +0100

    Create UserInfo.js component

---
+++ src/components/UserInfo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class UserInfo extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Name : {this.props.name}</p>
+        <p>Email : {this.props.email}</p>
+        <p>Website : {this.props.website}</p>
+      </div>
+    );
+  }
+}
+
+export default UserInfo;
