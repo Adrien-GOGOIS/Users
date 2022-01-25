bf92c975880120b4401b485ebef72ed4e5467575
Adrien GOGOIS <ad.gogois@gmail.com>
2022-01-25 16:13:52 +0100

    Finish users exercice without bonus

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import React from "react";
 import users from "./users.json";
 import UserInfo from "./components/UserInfo";
 
-class App extends React.Component {}
+const data = users.map((user) => {
+  return (
+    <UserInfo name={user.name} email={user.email} website={user.website} />
+  );
+});
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>{data}</ul>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -4,9 +4,13 @@ class UserInfo extends React.Component {
   render() {
     return (
       <div>
-        <p>Name : {this.props.name}</p>
-        <p>Email : {this.props.email}</p>
-        <p>Website : {this.props.website}</p>
+        <ul>
+          <li>
+            <p>Name : {this.props.name}</p>
+            <p>Email : {this.props.email}</p>
+            <p>Website : {this.props.website}</p>
+          </li>
+        </ul>
       </div>
     );
   }
